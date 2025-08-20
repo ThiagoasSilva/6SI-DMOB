4b4d86ac7f0a39c568bc2ff98cec389c94844923
Thiago Silva <129080167+ThiagoasSilva@users.noreply.github.com>
2025-08-20 18:02:20 +0000

    Alteração de menu e metodo MostrarSaldo

--- A02_RevisaoJava_20-08-25/ContaCorrente.java
+++ A02_RevisaoJava_20-08-25/ContaCorrente.java
@@ -23,19 +23,19 @@ public class ContaCorrente {
     }
 
     /*
-        public ContaCorrente(float saldo, float ChequeEspecial){
-            this.saldo = saldo;
-            this.chequeEspecial = chequeEspecial;
-        }
-        public float getSaldo(){
-            return saldo;
-        }
-        public float getChequeEspecial(){
-            return chequeEspecial;
-        }
-    */
+     * public ContaCorrente(float saldo, float ChequeEspecial){
+     * this.saldo = saldo;
+     * this.chequeEspecial = chequeEspecial;
+     * }
+     * public float getSaldo(){
+     * return saldo;
+     * }
+     * public float getChequeEspecial(){
+     * return chequeEspecial;
+     * }
+     */
 
-    public void Menu(){
+    public void Menu() throws Exception {
         System.out.println("\n============= MENU ==============");
         System.out.println("=== Selecione uma opção =========");
         System.out.println("=== 0 - Verificar Saldo   |======");
@@ -44,45 +44,67 @@ public class ContaCorrente {
         System.out.println("=== 3 - Encerrar          |======");
         System.out.println("=================================");
         int opcao = sc.nextInt();
-        switch (opcao){
-            case 0:
-                System.out.println("Opção: "+opcao);
-                break;
-            case 1:
-                System.out.println("Opção: "+opcao);//solução para reaproveitamento já que todos os cases utilizam a mesmo lógica!!!
-                break;
-            case 2:
-                System.out.println("Opção: "+opcao);
-                break;
-            case 3:
-                System.out.println("Opção: "+opcao);
-                break;
-        }
 
+        boolean ligado = true;
+        while (ligado) {
+            switch (opcao) {
+                case 0:
+                    System.out.println("Mostrar Saldo");
+                    MostrarSaldo();
+                    Menu();
+                    break;
+                case 1:
+                    System.out.println("Realizar Depósito");
+                    Depositar();
+                    Menu();
+                    break;
+                case 2:
+                    System.out.println("Realizar Saque");
+                    Sacar();
+                    Menu();
+                    break;
+                case 3:
+                    System.out.println("Programa encerrado.");
+                    ligado = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    Menu();
+                    break;
+            }
+        }
     }
-    public void Sacar() throws Exception{
+
+    public void Sacar() throws Exception {
         System.out.println("Insira um valor para sacar: ");
         float valor = sc.nextFloat();
 
-        if (valor<=0) {
+        if (valor <= 0) {
             throw new Exception("\n--- Operação Cancelada! valor zerado ou negativo \n--- Saldo: R$" + valor);
-        } else if(valor > (saldo + chequeEspecial)){
+        } else if (valor > (saldo + chequeEspecial)) {
             throw new Exception("\n--- Saldo Insuficiente! \n--- Saldo: R$" + (saldo + chequeEspecial));
         } else {
             saldo -= valor;
-            System.out.println("\n--- Saque realizado com sucesso!\n ---Valor do saque: R$" + valor + "\n--- Saldo atual: R$" + saldo);
+            System.out.println("\n--- Saque realizado com sucesso!\n ---Valor do saque: R$" + valor
+                    + "\n--- Saldo atual: R$" + saldo);
         }
     }
-    public void Depositar() throws Exception{
+
+    public void Depositar() throws Exception {
         System.out.println("Insira um valor para depositar: ");
         float deposito = sc.nextFloat();
 
-        if(deposito <= 0){
+        if (deposito <= 0) {
             throw new Exception("\n--- Depósito não realizado! Valor nulo ou negativo");
         } else {
-            saldo+=deposito;
-            System.out.println("\n--- Depósito realizado com sucesso! \n--- Valor depositado: R$"+deposito+" \n--- Saldo atual: R$ "+deposito);
+            saldo += deposito;
+            System.out.println("\n--- Depósito realizado com sucesso! \n--- Valor depositado: R$" + deposito
+                    + " \n--- Saldo atual: R$ " + saldo);
         }
+    }
+
+    public void MostrarSaldo() {
+        System.out.println("\n--- Saldo Atual: R$" + saldo);
     }
     // CONSTRUIR UM MÉTODO PARA TRANSFERIR valores entrs contas
 }
