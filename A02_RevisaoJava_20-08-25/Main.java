ace1d7a21bb1b7626050295e0390398b4b46ec8d
Thiago Silva <129080167+ThiagoasSilva@users.noreply.github.com>
2025-08-20 21:51:21 -0300

    Update Main.java

--- A02_RevisaoJava_20-08-25/Main.java
+++ A02_RevisaoJava_20-08-25/Main.java
@@ -1,8 +1,25 @@
-
 public class Main {
-    public static void main( String[] args) throws Exception {
-        ContaCorrente cc = new ContaCorrente();
-        cc.Menu();
+    public static void main(String[] args) {
+        ContaCorrente minhaConta = new ContaCorrente();
+        minhaConta.setSaldo(1000.0f);
+        minhaConta.setChequeEspecial(500.0f);
 
+        ContaCorrente contaDestino = new ContaCorrente();
+        contaDestino.setSaldo(200.0f);
+        contaDestino.setChequeEspecial(100.0f);
+        
+        System.out.println("Saldos Iniciais:");
+        System.out.println("Minha Conta: R$" + minhaConta.getSaldo());
+        System.out.println("Conta do Amigo: R$" + contaDestino.getSaldo());
+
+        System.out.println("\n--- Realizando Transferência ---");
+        
+        try {
+            float valorDaTransferencia = 300.0f;
+            minhaConta.transferir(contaDestino, valorDaTransferencia);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
