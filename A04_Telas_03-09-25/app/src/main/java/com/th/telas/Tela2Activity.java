653f771533175fc3b324712752c556fa5b6c85b0
thiagoassilva <thiagoantonio2108@gmail.com>
2025-09-03 10:26:20 -0300

    Atividade Telas concluida

--- A04_Telas_03-09-25/app/src/main/java/com/th/telas/Tela2Activity.java
+++ A04_Telas_03-09-25/app/src/main/java/com/th/telas/Tela2Activity.java
@@ -1,6 +1,8 @@
 package com.th.telas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Tela2Activity extends AppCompatActivity {
 
+    Button btTelaprincipal;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela2);
+
+        btTelaprincipal = (Button) findViewById(R.id.btTelaprincipal);
+        btTelaprincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Tela2Activity.this.finish();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
