5e36f2c55d2bc2dd16c09894f12a89bc776b4b17
thiagoassilva <thiagoantonio2108@gmail.com>
2025-09-03 09:47:03 -0300

    parte1 - telas 1 e 2, referencia a tela 2

--- A04_Telas_03-09-25/app/src/main/java/com/th/telas/Tela2Activity.java
+++ A04_Telas_03-09-25/app/src/main/java/com/th/telas/Tela2Activity.java
@@ -1,9 +1,6 @@
 package com.th.telas;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-
-    Button btTela2;
-    Intent iTela2;
+public class Tela2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        btTela2 = (Button) findViewById(R.id.bttela2);
-        btTela2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                iTela2 = new Intent(MainActivity.this, Tela2Activity.class);
-                startActivity(iTela2);
-
-            }
-        });
-
+        setContentView(R.layout.activity_tela2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
