9a5d7c44cb3f8588cf611aa22238ed25e1bc771b
thiagoassilva <thiagoantonio2108@gmail.com>
2025-11-03 21:26:14 -0300

    Criacao de database com execSQL

--- ProjetoMobile/app/src/main/java/com/dev/projetoantonius/MainActivity.java
+++ ProjetoMobile/app/src/main/java/com/dev/projetoantonius/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dev.projetoantonius;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,11 +15,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btcriarbanco;
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btcriarbanco = findViewById(R.id.btcriarbanco);
+        btcriarbanco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    db = openOrCreateDatabase("banco_dados",
+                            Context.MODE_PRIVATE, null);
+                    db.execSQL("create table if not exists "+
+                            " usuarios(numreg integer primary key "+
+                            " autoincrement, nome text not null, telefone text "+
+                            " not null, " + " email text not null) ");
+                    AlertDialog.Builder dialogo = new
+                            AlertDialog.Builder(MainActivity.this);
+                    dialogo.setTitle("Aviso")
+                            .setMessage("Banco de dados criado com sucesso!")
+                            .setNeutralButton("OK",  null)
+                            .show();
+                } catch (Exception e) {
+                }
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
