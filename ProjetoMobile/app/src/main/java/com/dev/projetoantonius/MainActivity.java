50aae00174dd007b8206da2bbe126391474c156e
thiagoassilva <thiagoantonio2108@gmail.com>
2025-11-11 22:08:02 -0300

    app rodando com erro na consulta e sem delete no crud

--- ProjetoMobile/app/src/main/java/com/dev/projetoantonius/MainActivity.java
+++ ProjetoMobile/app/src/main/java/com/dev/projetoantonius/MainActivity.java
@@ -10,9 +10,6 @@ import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
